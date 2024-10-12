2c66960a78fa95ab868157d63f3641d5eb08b3ae
Filippo Albertini <filippo@filippoalbertini.it>
2024-10-12 08:22:18 +0200

    Aggiungere i file di progetto.

---
+++ AS2425.3F.Prof.NoleggioAuto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AS2425._3F.Prof.NoleggioAuto
+{
+    /// <summary>
+    /// Dati i kilometri finali e iniziali, il nome del conducente ed un importo chilometrico 
+    /// fisso di € 1,70 calcolare il totale da pagare
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prof;3F;12/10/24;Calcolo importo noleggio auto");
+        }
+    }
+}
